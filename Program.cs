232882f426641c3d9aff870a70cc6d5fa4f1bb19
OlegPrg <115731500+OlegPrg@users.noreply.github.com>
2022-10-14 07:35:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var mgr = new UpdateManager("http://wwwclawv.beget.tech/dst2"))
+                {
+                    mgr.UpdateApp();
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+
+            Worker w = new Worker();
+            w.Start();
+        }
+    }
+}
